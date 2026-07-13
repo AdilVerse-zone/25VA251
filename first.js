586e95a2adc05ca97b5454172325694ec0036430
AdilVerse-zone <adilsohel849@gmail.com>
2026-07-13 12:40:45 +0530

    new

---
+++ first.js
@@ -0,0 +1,17 @@
+console.log("Adil is my name");
+console.log("i love javascript");
+
+name="Thor";
+age="1500";
+x="null";
+y = undefined;
+isfollow="false"
+name="25"
+let fullName="adilsohel" //or can be var const( avoid using var cuz of redeclaration again and again)
+console.log(fullName);//dynamic type
+//variable is case senstive
+//no space is allowed in variable writing
+//first character must be letter doller and underscore
+//reserved word cant be variable name
+//camelcase(first letter captial of each word)
+
