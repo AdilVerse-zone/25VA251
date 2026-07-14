f0a9cb86d44904fe528236cd8e2106899f75e7ea
AdilVerse-zone <adilsohel849@gmail.com>
2026-07-14 21:19:11 +0530

    new

---
+++ condition.js
@@ -0,0 +1,20 @@
+console.log("hello");
+
+let a=5;
+let b=6;
+console.log("a=",a,"b=",b)
+
+//normal version(==,!=)strict version (===,!==)
+//all kinds of oprators in c++ same in this
+
+//conditonal statement
+let age=25;
+if(age>=18){
+    console.log("true");
+
+}
+else(
+    console.log("false")
+)
+
+
---
+++ practice2.js
@@ -0,0 +1,8 @@
+let num = prompt("enter a number");
+
+if(num%5===0) {
+    console.log("this is multiple of 5");
+}
+else{
+    console.log("not a multiple of 5");
+}
