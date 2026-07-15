26e5b899d2b6bb4ff79e5316eb344ab558d2decd
AdilVerse-zone <adilsohel849@gmail.com>
2026-07-15 14:38:25 +0530

    new

--- practice2.js
+++ practice2.js
@@ -1,8 +1,10 @@
-let num = prompt("enter a number");
+// let num = prompt("enter a number");
 
-if(num%5===0) {
-    console.log("this is multiple of 5");
-}
-else{
-    console.log("not a multiple of 5");
-}
+// if(num%5===0) {
+//     console.log("this is multiple of 5");
+// }
+// else{
+//     console.log("not a multiple of 5");
+// }
+
+
