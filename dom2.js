015f291143b970abb4fb79b40d44a629c6ab96cb
AdilVerse-zone <adilsohel849@gmail.com>
2026-08-04 13:11:07 +0530

    new

--- dom2.js
+++ dom2.js
@@ -1,41 +1,73 @@
-// console.log("hello")
-// let heading = document.getElementById("header");
-// console.log("heading");
-// console.dir(window.document.body);
-// console.log(document.body);
-// console.log(document.head);
+// // console.log("hello")
+// // let heading = document.getElementById("header");
+// // console.log("heading");
+// // console.dir(window.document.body);
+// // console.log(document.body);
+// // console.log(document.head);
 
-// let headings = document.getElementsByClassName("heading");
-// console.dir(headings)
-// console.log(headings)
-
-
-// let parahs = document.getElementsByTagName("p");
-// console.dir(parahs);
+// // let headings = document.getElementsByClassName("heading");
+// // console.dir(headings)
+// // console.log(headings)
 
 
-// let elements = document.querySelector("p");
-// console.dir(elements)
+// // let parahs = document.getElementsByTagName("p");
+// // console.dir(parahs);
+
+
+// // let elements = document.querySelector("p");
+// // console.dir(elements)
+
+// // let div = document.querySelector("div")
+// // console.dir(div);
+// //tag name 
+// // inner text
+
+// let divs = document.querySelectorAll(".box");
+// // console.log(divs[0]);
+
+// let idx = 1;
+// for(div of divs){
+//     div.innerText = `new unique value ${idx}`;
+//     idx++;
+// }
+
+
+
+
+
+
+// // let h2 = document.querySelector("h2")
+// // console.dir(h2.innerText);
+// // h2.innerText = h2.innerText + " " + "from adil sohel";
+
 
 // let div = document.querySelector("div")
-// console.dir(div);
-//tag name 
-// inner text
+// console.log(div);
+// div.style.backgroundColor = "green"
+// div.style.fontSize = "20px"
 
-let divs = document.querySelectorAll(".box");
-// console.log(divs[0]);
+// let id = div.getAttribute("id");
+// console.log(id);
 
-let idx = 1;
-for(div of divs){
-    div.innerText = `new unique value ${idx}`;
-    idx++;
-}
+// let newBtn = document.createElement("button")
+// newBtn.innerText = "click me";
+// console.log(newBtn);
 
-
-
+// let div = document.querySelector("div");
+// div.append(newBtn)
 
 
 
-// let h2 = document.querySelector("h2")
-// console.dir(h2.innerText);
-// h2.innerText = h2.innerText + " " + "from adil sohel";
+// let newheading = document.createElement("h1");
+// newheading.innerHtml = "<i>hi,my name is Adil Sohel</i>";
+// document.querySelector("body").prepand(newheading);
+
+let newBtn = document.createElement("button");
+newBtn.innerHTML = "click me";
+newBtn.style.color = "white";
+newBtn.style.backgroundColor = "red";
+ document.querySelector("body").prepend(newBtn);
+
+
+ let para = document.querySelector("p")
+ 
